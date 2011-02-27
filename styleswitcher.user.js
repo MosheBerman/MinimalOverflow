3ce078f958b74f6ba3027379b4a16ffef110719d
Moshe <moshberm@gmail.com>
2011-02-26 20:24:40 -0500

    refactored switcher to prepare for UI

--- styleswitcher.user.js
+++ styleswitcher.user.js
@@ -4,7 +4,7 @@
 //
 // @namespace	http://mosheberman.com
 // @description   Load a stylesheet into a website
-// @version 0.0.5.1
+// @version 0.0.5.2
 //
 // @include      http://stackoverflow.com/*
 // @include      http://meta.stackoverflow.com/*
@@ -34,15 +34,37 @@
 
 //This URL will be loaded as the page's stylesheet and this overrides any other stylesheets as of now
 //var pathToStylesheet = "http://mosheberman.com/clean.css";
-var pathToStylesheet = "https://github.com/MosheBerman/MinimalOverflow/raw/master/clean.css"
+var pathToNewStylesheet = "https://github.com/MosheBerman/MinimalOverflow/raw/master/clean.css"
+
+var pathToOldStylesheet = "";
+
 //
 //	TODO: Build Switcher UI 
 //
 
-var linkElements = document.getElementsByTagName("link");
+function switchToStyleSheet(sheet){
+
+	//store a reference to the link elements on the page
+	var linkElements = document.getElementsByTagName("link");
+
+	for(var i=0; i<linkElements.length; i++){	
+		if(linkElements[i].getAttribute("rel").toString() == "stylesheet"){
 	
-for(var i=0; i<linkElements.length; i++){	
-	if(linkElements[i].getAttribute("rel").toString() == "stylesheet"){
-		linkElements[i].href = pathToStylesheet;
+			//If the path to the old stylesheet hasn't been set,	
+			//store it for use later
+			if(pathToOldStylesheet == ""){
+				pathToOldStylesheet = linkElements[i].href;
+			}
+		
+			//apply the new stylesheet
+			linkElements[i].href = pathToNewStylesheet;
+			
+			//We only want to replace the first stylesheet, so return.
+			//If SO decides to override anything here, just put it in a 
+			//second stylesheet and link tag.
+			return;
+		}
 	}
 }
+
+switchToStylesheet(pathToNewStylesheet);
