0a06e520e06f41b16ff6ff58e28663699399f440
Moshe <moshberm@gmail.com>
2011-02-26 19:20:54 -0500

    applied userscript to all of SE and moved CSS hosting to github

--- styleswitcher.user.js
+++ styleswitcher.user.js
@@ -4,15 +4,40 @@
 //
 // @namespace	http://mosheberman.com
 // @description   Load a stylesheet into a website
-// @version 0.0.4.9
+// @version 0.0.5
 //
-// @include	http://stackoverflow.com/
+// @include      http://stackoverflow.com/*
+// @include      http://meta.stackoverflow.com/*
+// @include      http://superuser.com/*
+// @include      http://meta.superuser.com/*
+// @include      http://serverfault.com/*
+// @include      http://meta.serverfault.com/*
+// @include      http://askubuntu.com/*
+// @include      http://meta.askubuntu.com/*
+// @include      http://answers.onstartups.com/*
+// @include      http://meta.answers.onstartups.com/*
+// @include      http://nothingtoinstall.com/*
+// @include      http://meta.nothingtoinstall.com/*
+// @include      http://seasonedadvice.com/*
+// @include      http://meta.seasonedadvice.com/*
+// @include      http://stackapps.com/*
+// @include      http://*.stackexchange.com/*
+// @exclude      http://chat.stackexchange.com/*
+// @exclude      http://chat.*.stackexchange.com/*
+// @exclude      http://api.*.stackexchange.com/*
+// @exclude      http://data.stackexchange.com/*
+// @exclude      http://*/reputation
 //
+// @author Moshe Berman
 //
 // ==/UserScript==
 
 //This URL will be loaded as the page's stylesheet and this overrides any other stylesheets as of now
-var pathToStylesheet = "http://mosheberman.com/clean.css";
+var pathToStylesheet = "https://github.com/MosheBerman/MinimalOverflow/blob/master/clean.css";
+
+//
+//	TODO: Build Switcher UI 
+//
 
 var linkElements = document.getElementsByTagName("link");
 	
